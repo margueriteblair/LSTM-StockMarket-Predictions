0931dc428bb393954b81038735ed6bf17b67d632
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-14 22:30:52 -0500

    set up a conditional where data can either be loaded from alphavantage or kaggle

--- main.py
+++ main.py
@@ -26,3 +26,16 @@ if data_source == 'alphavantage':
             data = json.loads(url.read().decode())
             data = data['Time Series (Daily)']
             df = pd.DataFrame(columns=['Date', 'Low', 'High', 'Close', 'Open'])
+            for k, v in data.item():
+                date = dt.datetime.strptime(k, '%Y-%m-%d')
+                data_row = [date.date(), float(v['3. low']), float(v['2. high']), float(v['4. close']), float(v['1. open'])]
+                df.loc[-1,:] = data_row
+                df.index = df.index + 1
+            print("Data saved to : %s"%file_to_save)
+            df.to_csv(file_to_save)
+    else:
+        print("File already exists. Leading data from CSV.")
+        df = pd.read_csv(file_to_save)
+else:
+    df = pd.read_csv(os.path.join('Stocks', 'hpq.us.txt'), delimiter=',', usecols=['Date', 'Open', 'High', 'Low', 'Close'])
+    print("Loaded data from the Kaggle repository")
