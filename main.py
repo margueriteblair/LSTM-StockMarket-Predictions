201396fbc7f65b5f3c0aec7d8ef91c61d50a74ea
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-23 18:58:44 -0500

    replaced date var with xticks

--- main.py
+++ main.py
@@ -59,17 +59,18 @@ std_avg_x = []
 mse_errors = []
 
 for pred_idx in range(window_size, N):
-    date = df.loc[pred_idx, 'Date']
+    # date = df.loc[pred_idx, 'Date']
 
     std_avg_predictions.append(np.mean(train_data[pred_idx - window_size:pred_idx]))
     mse_errors.append((std_avg_predictions[-1] - train_data[pred_idx]) ** 2)
-    std_avg_x.append(date)
+    # std_avg_x.append(date)
 
 print('MSE error for standard averaging: %.5f' % (0.5 * np.mean(mse_errors)))
 plt.figure(figsize=(18, 9))
 plt.plot(range(df.shape[0]), all_mid_data, color='b', label='True')
 plt.plot(range(window_size, N), std_avg_predictions, color='orange', label='Prediction')
 plt.title('SMA Prediction vs Actual')
+plt.xticks(range(0, df.shape[0], 500), df['Date'].loc[::500], rotation=45)
 plt.xlabel('Date')
 plt.ylabel('Mid Price')
 plt.legend(fontsize=18)
@@ -90,12 +91,13 @@ for pred_idx in range(1, N):
     running_mean = running_mean * decay + (1.0 - decay) * train_data[pred_idx - 1]
     run_avg_predictions.append(running_mean)
     mse_errors.append((run_avg_predictions[-1] - train_data[pred_idx]) ** 2)
-    run_avg_x.append(date)
+    # run_avg_x.append(date)
 
 print('MSE error for EMA averaging: %.5f' % (0.5 * np.mean(mse_errors)))
 plt.figure(figsize=(18, 9))
 plt.plot(range(df.shape[0]), all_mid_data, color='b', label='True')
 plt.plot(range(0, N), run_avg_predictions, color='orange', label='Prediction')
+plt.xticks(range(0, df.shape[0], 500), df['Date'].loc[::500], rotation=45)
 plt.title('EMA Prediction vs Actual')
 plt.xlabel('Date', fontsize=18)
 plt.ylabel('Mid Price', fontsize=18)
