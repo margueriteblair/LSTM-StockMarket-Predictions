03355c236e54acf940330c0b89b34816e5c91ef4
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-23 21:04:59 -0500

    adding ability to make point predictions about where a price will be with the simple average

--- main.py
+++ main.py
@@ -53,22 +53,31 @@ for ti in range(split):
 all_mid_data = np.concatenate([train_data, test_data], axis=0)
 
 window_size = 100
-N = all_mid_data.size+200
+N = train_data.size
 std_avg_predictions = []
 std_avg_x = []
 mse_errors = []
 
 for pred_idx in range(window_size, N):
-
+    date = df.loc[pred_idx, 'Date']
     std_avg_predictions.append(np.mean(train_data[pred_idx - window_size:pred_idx]))
     mse_errors.append((std_avg_predictions[-1] - train_data[pred_idx]) ** 2)
+    std_avg_x.append(date)
 
 print('MSE error for standard averaging: %.5f' % (0.5 * np.mean(mse_errors)))
+
+#In other words, you say the prediction at t+1 is the average value of all the stock prices you observed within a window of t to t−N.
+# print(7125-N)
+t_plus_one = 7125
+diff = t_plus_one - (t_plus_one-N)
+next_day = np.mean(all_mid_data[diff: t_plus_one])
+print(next_day)
 plt.figure(figsize=(18, 9))
+plt.plot(7125, next_day, 'ro')
 plt.plot(range(df.shape[0]), all_mid_data, color='b', label='True')
 plt.plot(range(window_size, N), std_avg_predictions, color='orange', label='Prediction')
 plt.title('SMA Prediction vs Actual')
-plt.xticks(range(0, df.shape[0], 500), df['Date'].loc[::500], rotation=45)
+# plt.xticks(range(0, df.shape[0], 500), df['Date'].loc[::500], rotation=45)
 plt.xlabel('Date')
 plt.ylabel('Mid Price')
 plt.legend(fontsize=18)
